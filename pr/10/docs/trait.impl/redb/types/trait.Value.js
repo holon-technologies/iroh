f50f00d0b0c2374ee8c4808de00e3416e3c00be5
jonaswre <jonaswre@users.noreply.github.com>
2026-07-30 20:31:02 +0000

    deploy: 3f6101897dd3373a5248b06a82263f0a86ea673e

---
+++ pr/10/docs/trait.impl/redb/types/trait.Value.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["krikos_blobs",[["impl Value for <a class=\"struct\" href=\"krikos_blobs/struct.Hash.html\" title=\"struct krikos_blobs::Hash\">Hash</a>",0],["impl Value for <a class=\"struct\" href=\"krikos_blobs/struct.HashAndFormat.html\" title=\"struct krikos_blobs::HashAndFormat\">HashAndFormat</a>",0],["impl Value for <a class=\"struct\" href=\"krikos_blobs/api/struct.Tag.html\" title=\"struct krikos_blobs::api::Tag\">Tag</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[426]}
---
+++ pr/10/docs/trait.impl/tokio/io/async_read/trait.AsyncRead.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["krikos_blobs",[["impl AsyncRead for <a class=\"struct\" href=\"krikos_blobs/api/blobs/struct.BlobReader.html\" title=\"struct krikos_blobs::api::blobs::BlobReader\">BlobReader</a>",0]]],["krikos_relay",[["impl AsyncRead for <a class=\"enum\" href=\"krikos_relay/server/streams/enum.MaybeTlsStream.html\" title=\"enum krikos_relay::server::streams::MaybeTlsStream\">MaybeTlsStream</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[187,204]}
