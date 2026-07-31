36c6c0e9e01af63dce01832d0239308e8a3a1da0
jonaswre <jonaswre@users.noreply.github.com>
2026-07-31 23:26:36 +0000

    deploy: f789d57649b1e239769d588ec91dab59d5574cce

---
+++ pr/16/docs/krikos_relay/http/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"constant":["CLIENT_AUTH_HEADER","RELAY_PATH","RELAY_PROBE_PATH","X_IROH_ENDPOINT_ID"],"enum":["ProtocolVersion"],"struct":["UnsupportedRelayProtocolVersion"]};
