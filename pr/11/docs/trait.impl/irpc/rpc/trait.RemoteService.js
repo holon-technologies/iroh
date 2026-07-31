c3b2b509b3ca2f83275d0313ef0f3b697ac2ba2e
jonaswre <jonaswre@users.noreply.github.com>
2026-07-31 00:53:22 +0000

    deploy: 9976a24c66b6133dc9ad921e800d94c66710f2e4

---
+++ pr/11/docs/trait.impl/irpc/rpc/trait.RemoteService.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["krikos_blobs",[["impl RemoteService for <a class=\"enum\" href=\"krikos_blobs/provider/events/enum.ProviderProto.html\" title=\"enum krikos_blobs::provider::events::ProviderProto\">ProviderProto</a>",0]]],["krikos_docs",[["impl RemoteService for <a class=\"enum\" href=\"krikos_docs/api/protocol/enum.DocsProtocol.html\" title=\"enum krikos_docs::api::protocol::DocsProtocol\">DocsProtocol</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[206,195]}
