649701a997edf5949d2de150d8751018bb6f6aa7
jonaswre <jonaswre@users.noreply.github.com>
2026-08-01 22:35:04 +0000

    deploy: 68efcfc2d0c5497b3df1753ca0cde974ba8fadb2

---
+++ pr/22/docs/trait.impl/core/iter/traits/exact_size/trait.ExactSizeIterator.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["krikos",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/exact_size/trait.ExactSizeIterator.html\" title=\"trait core::iter::traits::exact_size::ExactSizeIterator\">ExactSizeIterator</a> for <a class=\"struct\" href=\"krikos/endpoint/struct.PathListIter.html\" title=\"struct krikos::endpoint::PathListIter\">PathListIter</a>&lt;'_&gt;",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[379]}
