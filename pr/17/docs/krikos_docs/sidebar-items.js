0970a01d8ae52dee4f8e87ec223f2b91f84b2812
jonaswre <jonaswre@users.noreply.github.com>
2026-08-01 00:18:57 +0000

    deploy: 9427342a3f780e4d744eaeb8f1adb1b81565d68e

---
+++ pr/17/docs/krikos_docs/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"constant":["ALPN"],"enum":["ParseError"],"mod":["actor","api","engine","limits","metrics","net","protocol","store","sync"],"struct":["Author","AuthorHeads","AuthorId","AuthorPublicKey","DocTicket","NamespaceId","NamespacePublicKey","NamespaceSecret"]};
---
+++ pr/17/docs/trait.impl/postcard/max_size/trait.MaxSize.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["krikos_blobs",[["impl MaxSize for <a class=\"enum\" href=\"krikos_blobs/enum.BlobFormat.html\" title=\"enum krikos_blobs::BlobFormat\">BlobFormat</a>",0],["impl MaxSize for <a class=\"struct\" href=\"krikos_blobs/struct.Hash.html\" title=\"struct krikos_blobs::Hash\">Hash</a>",0],["impl MaxSize for <a class=\"struct\" href=\"krikos_blobs/struct.HashAndFormat.html\" title=\"struct krikos_blobs::HashAndFormat\">HashAndFormat</a>",0],["impl MaxSize for <a class=\"enum\" href=\"krikos_blobs/protocol/enum.RequestType.html\" title=\"enum krikos_blobs::protocol::RequestType\">RequestType</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[599]}
