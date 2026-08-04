9a5c3e85ce0d7bf18af204d64d054c0660f1f0a5
jonaswre <jonaswre@users.noreply.github.com>
2026-08-04 14:41:30 +0000

    deploy: 154b66082edf40b8716865ec57fdcd8b46ef19ef

---
+++ pr/24/docs/trait.impl/rustls_acme/cache/trait.AccountCache.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["krikos_relay",[["impl AccountCache for <a class=\"struct\" href=\"krikos_relay/server/struct.BoundedAcmeCache.html\" title=\"struct krikos_relay::server::BoundedAcmeCache\">BoundedAcmeCache</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[201]}
